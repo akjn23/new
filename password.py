6ea214e755fa350508546c1cb32ce3337de6906a
akjn23 <55597629+akjn23@users.noreply.github.com>
2019-10-25 10:27:55 +0530

    Add files via upload

---
+++ password.py
@@ -0,0 +1,41 @@
+import string
+import random
+
+
+def passtyyyy():
+
+     
+     nouns = ['apple', 'dinosaur', 'ball',
+     'toaster', 'goat', 'dragon',
+     'hammer', 'duck', 'panda']
+
+     adjectives = ['sleepy', 'slow', 'smelly',
+     'wet', 'fat', 'red',
+     'orange', 'yellow', 'green',
+     'blue', 'purple', 'fluffy',
+     'white', 'proud', 'brave']
+
+     noun=random.choice(nouns)
+     adjective=random.choice(adjectives)
+     number=str(random.randrange(0,100))
+     string_p=random.choice(string.punctuation)
+     password=noun+adjective+number+string_p 
+
+
+     return password
+
+print('password finder')
+while True:
+    pas=passtyyyy()
+    print('Your new password is: ', pas)
+    print('like password!')
+    ans = input('enter yes or no')
+    if ans=='yes':
+        break
+
+
+
+    
+
+
+
